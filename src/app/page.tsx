5c7c8ce90339398623f8264b67c31e4f7ec91744
Sergio <sergio.costa_ads@outlook.com>
2025-03-02 17:27:30 +0000

    adicionado NEXT_PUBLIC_CLERK_FRONTEND_API=<your-clerk-publishable-key>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default async function HomePage() {
           <SignUpButton />
         </Button>
         <UserButton />
-        <p>tteste</p>
+        <p>Teste</p>
       </div>
     </div>
   )
