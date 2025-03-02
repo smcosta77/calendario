b58e202b018c5a3fbef69c00b89481be2554aa97
Sergio <sergio.costa_ads@outlook.com>
2025-03-02 17:38:52 +0000

    nova atualizado para deploy no vercel

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_CLERK_FRONTEND_API: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API,
+  },
 };
 
 export default nextConfig;
