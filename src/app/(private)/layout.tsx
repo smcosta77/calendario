29730c02b9ab21d73bb6257fa9215c6a523d8dac
Sergio <sergio.costa_ads@outlook.com>
2025-03-02 15:40:49 +0000

    teste commit

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -7,7 +7,7 @@ export default function PrivateLayut({ children }: { children: ReactNode }) {
     <>
       <header className="flex py-2 border-b bg-card">
         <nav className="font-medium flex items-center text-sm gap-6 container mx-auto">
-          <div></div>
+          <div>Agora vamos ver</div>
         </nav>
       </header>
       <main className="container my-6 mx-auto">
