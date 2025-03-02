8d4612303233ff2f16557a42a5d625d750fa11bd
Sergio <sergio.costa_ads@outlook.com>
2025-03-02 18:46:19 +0000

    bla bla

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  },
   
 };
 
 export default nextConfig;
-
-module.exports = {
-  env: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-  },
-};
